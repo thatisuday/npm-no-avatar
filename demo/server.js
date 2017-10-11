b676222243c09f5334cb43110fc5e1fefd63a1c8
Uday Hiwarale <thatisuday@users.noreply.github.com>
2017-10-12 01:37:25 +0530

    Update server.js

--- demo/server.js
+++ demo/server.js
@@ -4,6 +4,6 @@ const { middleware } =  require(__dirname + '/../');
 
 app.get('/avatar.png', middleware);
 
-app.listen(3000, function(){
-	console.log('no-avatar server is listening on port 3000. Visit http://localhost:3000/avatar.png?text=UH&fontSize=80');
+app.listen(80, function(){
+	console.log('no-avatar server is listening on port 3000. Visit http://localhost:80/avatar.png?text=UH&fontSize=80');
 });
